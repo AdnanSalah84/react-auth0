4608441840a398443ddc100fa926eb6927a89075
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-07-25 16:18:13 +0500

    Add nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
+import Nav from './Nav';
+
 
 function App() {
   return (
     <>
-    <Route path="/" exact component={Home} />
-    <Route path="/profile" component={Profile} />
+      <Nav />
+      <Route path="/" exact component={Home} />
+      <Route path="/profile" component={Profile} />
     </>
   );
 }
