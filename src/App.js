8552ea39564a22b24e8d258027aeb2cf232e1f63
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-07-31 16:10:09 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+//import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
@@ -7,30 +8,45 @@ import Callback from './Callback';
 import Auth from './Auth/Auth';
 import Public from './Public';
 import Private from './Private';
-import Courses from './Courses';
+//import Courses from './Courses';
 import PrivateRoute from './PrivateRoute';
+import AuthContext from './AuthContext';
+import Courses from './Courses';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.auth = new Auth(this.props.history);
+    //this.auth = new Auth(this.props.history);
+    this.state = {
+      auth: new Auth(this.props.history),
+      tokenRenewalComplete: false
+    }
+  }
+
+  componentDidMount() {
+    this.state.auth.renewToken(() => {
+      this.setState({ tokenRenewalComplete: true });
+    })
   }
 
   render() {
+    const { auth } = this.state;
+    // Show loading message until the token renewal check is completed.
+    if (!this.state.tokenRenewalComplete) return "Loading...";
     return (
-      <>
-        <Nav auth={this.auth} />
+      <AuthContext.Provider value={auth}>
+        <Nav auth={auth} />
         <div className="body">
           {/* <Route path="/" exact component={Home} /> */}
           {/* <Route path="/profile" component={Profile} /> */}
           <Route
             path="/"
             exact
-            render={props => <Home auth={this.auth} {...props} />}
+            render={props => <Home auth={auth} {...props} />}
           />
           <Route
             path="/callback"
-            render={props => <Callback auth={this.auth} {...props} />}
+            render={props => <Callback auth={auth} {...props} />}
           />
           {/* <Route
             path="/profile"
@@ -42,10 +58,10 @@ class App extends Component {
                 )
             }
           /> */}
-          <PrivateRoute path="/profile" component={Profile} auth={this.auth} />
+          <PrivateRoute path="/profile" component={Profile} />
           <Route path="/public" component={Public} />
-          <PrivateRoute path="/private" component={Private} auth={this.auth} />
-          <PrivateRoute path="/course" component={Private} auth={this.auth} scopes={['read:courses']} />
+          <PrivateRoute path="/private" component={Private} />
+          <PrivateRoute path="/course" component={Courses} scopes={['read:courses']} />
           {/* <Route
             path="/private"
             render={props => this.auth.isAuthenticated() ?
@@ -69,7 +85,7 @@ class App extends Component {
             }
           /> */}
         </div>
-      </>
+      </AuthContext.Provider>
     );
   }
 }
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,6 +2,13 @@ import auth0 from 'auth0-js';
 
 const REDIRECT_ON_LOGIN = 'redirect_on_login';
 
+// Stored outside class since private
+// eslint-disable-next-line
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
 export default class Auth {
     constructor(history) {
         this.history = history;
@@ -42,33 +49,39 @@ export default class Auth {
     setSession = authResult => {
         console.log(authResult);
         // Set the time that the access token will expire\
-        const expiresAt = JSON.stringify(
-            authResult.expiresIn * 1000 + new Date().getTime()
-        );
+        // const expiresAt = JSON.stringify(
+        //     authResult.expiresIn * 1000 + new Date().getTime()
+        // );
+
+        _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
 
         // If there is a value on the `scope` param from the authResult,
         // use it to scopes in the session for the user. Otherwise
         // use the scopes as requested. if no scopes were requested,
         // set it to nothing.
-        const scopes = authResult.scope || this.requestedScopes || "";
+        _scopes = authResult.scope || this.requestedScopes || "";
 
-        localStorage.setItem('access_token', authResult.accessToken);
-        localStorage.setItem('id_token', authResult.idToken);
-        localStorage.setItem('expires_at', expiresAt);
-        localStorage.setItem('scopes', JSON.stringify(scopes));
+        _accessToken = authResult.accessToken;
+        _idToken = authResult.idToken;
+
+        // localStorage.setItem('access_token', authResult.accessToken);
+        // localStorage.setItem('id_token', authResult.idToken);
+        // localStorage.setItem('expires_at', expiresAt);
+        // localStorage.setItem('scopes', JSON.stringify(scopes));
+        this.scheduleTokenRenewal();
     }
 
     isAuthenticated() {
-        const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-        return new Date().getTime() < expiresAt;
+        //const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+        return new Date().getTime() < _expiresAt;
     }
 
     logout = () => {
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('id_token');
-        localStorage.removeItem('expires_at');
-        localStorage.removeItem('scopes');
-        this.userProfile = null;
+        // localStorage.removeItem('access_token');
+        // localStorage.removeItem('id_token');
+        // localStorage.removeItem('expires_at');
+        // localStorage.removeItem('scopes');
+        //this.userProfile = null;
         // this.history.push('/');
         this.auth0.logout({
             clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -77,11 +90,11 @@ export default class Auth {
     }
 
     getAccessToken = () => {
-        const accessToken = localStorage.getItem('access_token');
-        if (!accessToken) {
+        //const accessToken = localStorage.getItem('access_token');
+        if (!_accessToken) {
             throw new Error("No access token found");
         }
-        return accessToken;
+        return _accessToken;
     }
 
     getProfile = cb => {
@@ -92,9 +105,24 @@ export default class Auth {
     }
 
     userHasScopes(scopes) {
-        const grantedScopes = (
-            JSON.parse(localStorage.getItem("scopes")) || ""
-        ).split(" ");
+        //const grantedScopes = (JSON.parse(localStorage.getItem("scopes")) || "").split(" ");
+        const grantedScopes = (_scopes || "").split(" ");
         return scopes.every(scope => grantedScopes.includes(scope));
+    }
+
+    renewToken(cb) {
+        this.auth0.checkSession({}, (err, result) => {
+            if (err) {
+                console.log(`Error:${err.error} - ${err.error_description}.`);
+            } else {
+                this.setSession(result);
+            }
+            if (cb) cb(err, result);
+        })
+    }
+
+    scheduleTokenRenewal() {
+        const delay = _expiresAt - Date.now();
+        if (delay > 0) setTimeout(() => this.renewToken(), delay);
     }
 }
